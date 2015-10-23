10414d79e90e51efd0c0711dd47852d20482fdf6
cemolcay <ccemolcay@gmail.com>
2015-10-22 17:24:59 -0700

    Initial Commit

---
+++ ALKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALKit
+//
+//  Created by Cem Olcay on 22/10/15.
+//  Copyright © 2015 prototapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
